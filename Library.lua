f7c8fc620181f605fdb938d96ddb5dc600f5463b
matt <102203325+matas3535@users.noreply.github.com>
2023-09-12 23:03:57 +0100

    Update Library.lua
    i have autism

--- Library.lua
+++ Library.lua
@@ -1966,7 +1966,7 @@ local Notifications = {
 function Bracket:QueueNotification(Name, Duration, Color, Callback)
 	if Notifications.Last and Notifications.Last.Name == Name then
 		Notifications.Last.Count += 1
-		Notification.Last.Tick = tick()
+		Notifications.Last.Tick = tick()
 		--
 		return
 	end
