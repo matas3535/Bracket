439d72aefc787f091f394f9f225a6dd91da91c72
matt <102203325+matas3535@users.noreply.github.com>
2023-09-12 23:06:55 +0100

    Update Library.lua
    IM FCUKING RETARDED

--- Library.lua
+++ Library.lua
@@ -1,5 +1,5 @@
 local Color3_fromHex, Color3_fromHSV, Color3_fromRGB, Color3_new, coroutine_wrap, Instance_new, math_clamp, math_floor, os_clock, Random_new, string_find, string_format, string_gsub, string_rep, string_split, table_clear, table_concat, table_find, table_remove, task_spawn, task_wait, UDim2_fromScale, utf8_char, tonumber, loadstring, delfile, UDim2_new, sethiddenproperty, tostring, readfile, writefile, isfile, listfiles, makefolder, isfolder, math_max, OnStop, OnTick, UDim2_fromOffset, Vector2_new, setmetatable, ipairs, table_insert, typeof, type, pairs = Color3.fromHex, Color3.fromHSV, Color3.fromRGB, Color3.new, coroutine.wrap, Instance.new, math.clamp, math.floor, os.clock, Random.new, string.find, string.format, string.gsub, string.rep, string.split, table.clear, table.concat, table.find, table.remove, task.spawn, task.wait, UDim2.fromScale, utf8.char, tonumber, loadstring, delfile, UDim2.new, sethiddenproperty, tostring, readfile, writefile, isfile, listfiles, makefolder, isfolder, math.max, OnStop, OnTick, UDim2.fromOffset, Vector2.new, setmetatable, ipairs, table.insert, typeof, type, pairs
---
+-- a
 local Utility = {
 	Backgrounds = {
 		{"Legacy", "rbxassetid://2151741365"},
